c7818863125b8c44a86981c46ab55869fb2e7ad6
Mehak Sadique <mksd1999@gmail.com>
2020-08-16 13:11:53 -0400

    .

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
-app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
+const port = process.env.PORT || 5000;
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
